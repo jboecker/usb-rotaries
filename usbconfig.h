a146f2cac3a68877df3233d04eee7cd2c66935eb
Jan Böcker <jan.boecker@jboecker.de>
2012-05-04 00:03:14 +0200

    switch VID/PID to the shared pair for joysticks

--- usbconfig.h
+++ usbconfig.h
@@ -204,7 +204,7 @@ extern void hadUsbReset(void);
 
 /* -------------------------- Device Description --------------------------- */
 
-#define  USB_CFG_VENDOR_ID       0xc0, 0x16 /* = 0x16c0 = 5824 = voti.nl */
+#define  USB_CFG_VENDOR_ID       0xc0, 0x16 /* shared VID for USB Joysticks */
 /* USB vendor ID for the device, low byte first. If you have registered your
  * own Vendor ID, define it here. Otherwise you may use one of obdev's free
  * shared VID/PID pairs. Be sure to read USB-IDs-for-free.txt for rules!
@@ -213,7 +213,7 @@ extern void hadUsbReset(void);
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define  USB_CFG_DEVICE_ID       0xdb, 0x27 /* 0x27db = free PID for USB Keyboards */
+#define  USB_CFG_DEVICE_ID       0xdc, 0x27 /* shared PID for USB Joysticks */
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
