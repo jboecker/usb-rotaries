5b9bdaa6701f9d53ddc219d9bdb226b196c95a77
Jan Böcker <jan.boecker@jboecker.de>
2012-05-04 13:38:38 +0200

    usbconfig.h: unset USB_CFG_HAVE_MEASURE_FRAME_LENGTH

--- usbconfig.h
+++ usbconfig.h
@@ -189,7 +189,7 @@ extern void hadUsbReset(void);
  * usbFunctionWrite(). Use the global usbCurrentDataToken and a static variable
  * for each control- and out-endpoint to check for duplicate packets.
  */
-#define USB_CFG_HAVE_MEASURE_FRAME_LENGTH   1
+#define USB_CFG_HAVE_MEASURE_FRAME_LENGTH   0
 /* define this macro to 1 if you want the function usbMeasureFrameLength()
  * compiled in. This function can be used to calibrate the AVR's RC oscillator.
  */
