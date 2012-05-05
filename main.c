9bc43ec028fdfcbb60aa2f12df06ec3cdef44eb3
Jan Böcker <jan.boecker@jboecker.de>
2012-05-05 23:26:36 +0200

    add key-press functionality; remove X,Y,Z axis (otherwise linux thinks the keypresses are mouse buttons)

--- main.c
+++ main.c
@@ -22,22 +22,56 @@
 
 /* ------------------------------------------------------------------------- */
 
-static uchar    reportBuffer1[6] = {1,0,0,0,0,0};
+static uchar    reportBuffer1[3] = {1,0,0};
 static uchar    reportBuffer2[7] = {2,0,0,0,0,0,0};
+static uchar    reportBufferKbd[8] = {3,0,0,0,0,0,0,0};
+static uchar    newKbdReport = 0;
 
-PROGMEM const char usbHidReportDescriptor[71] = {
+PROGMEM const char usbHidReportDescriptor[142] = {
+	//PROGMEM const char usbHidReportDescriptor[65] = {
+
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x06,                    // USAGE (Keyboard)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0xa1, 0x00,                    // COLLECTION (Physical)
+    0x85, 0x03,                    // REPORT ID (3)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
+    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
+    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x08,                    //   REPORT_COUNT (8)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)	** Modifier Byte **
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)	** Reserved Byte **
+    0x95, 0x05,                    //   REPORT_COUNT (5)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x05, 0x08,                    //   USAGE_PAGE (LEDs)
+    0x19, 0x01,                    //   USAGE_MINIMUM (Num Lock)
+    0x29, 0x05,                    //   USAGE_MAXIMUM (Kana)
+    0x91, 0x02,                    //   OUTPUT (Data,Var,Abs)	** LED Report **
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x03,                    //   REPORT_SIZE (3)
+    0x91, 0x03,                    //   OUTPUT (Cnst,Var,Abs)	** LED Report Padding **
+    0x95, 0x05,                    //   REPORT_COUNT (5)		** here we define the maximum number of simultaneous keystrokes we can detect ** 
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
+    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
+    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)	** Key arrays (6 bytes) **
+	0xc0,                           // END_COLLECTION  
+	0xc0,                           // END_COLLECTION  
+
+	
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
     0x09, 0x04,                    // USAGE (Joystick)
-    0xa1, 0x01,                    // COLLECTION (Application)
+	0xa1, 0x01,                    // COLLECTION (Application)
+    0xa1, 0x00,                    // COLLECTION (Physical)
     0x85, 0x01,                    //   REPORT_ID (1)
-    0x09, 0x30,                    //   USAGE (X)
-    0x09, 0x31,                    //   USAGE (Y)
-    0x09, 0x32,                    //   USAGE (Z)
-    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
-    0x46, 0xff, 0x00,              //   PHYSICAL_MAXIMUM (255)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x03,                    //   REPORT_COUNT (3)
-    0x81, 0x02,                    // INPUT (Data,Var,Abs)
 
     0x05, 0x09,                    // USAGE_PAGE (Button)
     0x19, 0x01,                    // USAGE_MINIMUM (Button 1)
@@ -62,7 +96,9 @@ PROGMEM const char usbHidReportDescriptor[71] = {
     0x95, 0x06,                    //   REPORT_COUNT (6)
     0x81, 0x02,                    // INPUT (Data,Var,Abs)
 
-    0xc0                           // END_COLLECTION
+    0xc0,                           // END_COLLECTION
+	0xc0,                           // END_COLLECTION  
+
 };
 
 static uchar    idleRate;           /* in 4 ms units */
@@ -141,13 +177,15 @@ usbRequest_t    *rq = (void *)data;
     usbMsgPtr = NULL;
     if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
         if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
-			if (rq->wValue.bytes[1] == 1) { // look at the ReportID
+						if (rq->wValue.bytes[1] == 1) { // look at the ReportID
 				usbMsgPtr = reportBuffer1;
 				return sizeof(reportBuffer1);
-			} else {
+			} else if (rq->wValue.bytes[1] == 2) {
 				usbMsgPtr = reportBuffer2;
 				return sizeof(reportBuffer2);
-			}
+			} else if (rq->wValue.bytes[1] == 3) {
+				usbMsgPtr = reportBufferKbd;
+							}
         }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
             usbMsgPtr = &idleRate;
             return 1;
@@ -246,20 +284,20 @@ uchar   i;
 		events = encoder_events(oldstate, newstate);
 		if (events & ECEV_LEFT) axisValues[currentAxis]--;
 		if (events & ECEV_RIGHT) axisValues[currentAxis]++;
-		if (events & ECEV_BUTTON_DOWN) reportBuffer1[4] |= 1;
-		if (events & ECEV_BUTTON_UP) reportBuffer1[4] &= ~1;
+		if (events & ECEV_BUTTON_DOWN) reportBuffer1[1] |= 1;
+		if (events & ECEV_BUTTON_UP) reportBuffer1[1] &= ~1;
 	
 		events = encoder_events((oldstate >> 3), (newstate >> 3));
 		if (events & ECEV_LEFT) { currentAxis--; if (currentAxis == 255) currentAxis = 8; }
 		if (events & ECEV_RIGHT) { currentAxis++; if (currentAxis > 8) currentAxis = 0; }
-		if (events & ECEV_BUTTON_DOWN) reportBuffer1[4] |= 2;
-		if (events & ECEV_BUTTON_UP) reportBuffer1[4] &= ~2;
+		if (events & ECEV_BUTTON_DOWN) { reportBufferKbd[5] = axisValues[0]; newKbdReport = 0; }
+		if (events & ECEV_BUTTON_UP) { reportBufferKbd[5] = 0; newKbdReport = 0; }
 	
 		oldstate=newstate;
 		
-		reportBuffer1[1] = axisValues[0];
-        reportBuffer1[2] = axisValues[1];
-		reportBuffer1[3] = axisValues[2];
+		//		reportBuffer1[1] = axisValues[0];
+		//        reportBuffer1[2] = axisValues[1];
+		//		reportBuffer1[3] = axisValues[2];
 		
 		reportBuffer2[1] = axisValues[3];
 		reportBuffer2[2] = axisValues[4];
@@ -279,14 +317,17 @@ uchar   i;
 		
 		newReport = 0;
 		static uchar reportNumber = 1;
-		reportNumber++; if (reportNumber == 3) reportNumber = 1;
+		reportNumber++; if (reportNumber == 4) reportNumber = 1;
 		if(usbInterruptIsReady() && newReport == 0){ /* we can send another report */
-			if (reportNumber == 1)
+			if (reportNumber == 1) {
 				usbSetInterrupt(reportBuffer1, sizeof(reportBuffer1));
-			else
+			} else if (reportNumber == 2) {
 				usbSetInterrupt(reportBuffer2, sizeof(reportBuffer2));
-					
-        	}
+			} else if (reportNumber == 3 && newKbdReport == 0) {
+				usbSetInterrupt(reportBufferKbd, sizeof(reportBufferKbd));
+				newKbdReport = 1;
+			}
+		}
         
 		timerPoll();
 	}
